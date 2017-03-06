39bfb51cbc403893155038b0d36d8fe472ecbbfd
David Provest <dpro7026@uni.sydney.edu.au>
2017-03-06 14:45:37 +1100

    Implemented listing articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  # Order the articles with the newest first
+  default_scope { order(created_at: :desc) }
 end
